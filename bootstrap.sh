aa19ce1e1ff8199f164f23d9dece0552e5515902
Seiei Higa <hanachin@gmail.com>
2015-04-02 18:41:38 +0900

    Redisは使わない

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,6 @@ gem install bundler -N >/dev/null 2>&1
 
 install Git git
 install SQLite sqlite3 libsqlite3-dev
-install Redis redis-server
 install RabbitMQ rabbitmq-server
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
