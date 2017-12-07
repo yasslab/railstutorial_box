2e90f796ce818ba69e282d8bd12b88c633288720
Yoshiaki Oshiro <himajin315@gmail.com>
2017-12-07 14:23:19 +0900

    Update Ubuntu 17.10

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,20 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
-Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty32"
-  config.vm.network "forwarded_port", guest: 3000, host: 3000
-  config.vm.provision "shell", path: "bootstrap.sh", keep_color: true
+Vagrant.configure('2') do |config|
+  # FIXME: When upgrading to a future version of Ubuntu check if the workaround
+  # near the top of bootstrap.sh is still needed. If not, please delete it.
+  config.vm.box      = 'ubuntu/artful64' # 17.10
+  config.vm.hostname = 'rails-dev-box'
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+
+  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+
+  config.ssh.insert_key = false
+
+  config.vm.provider 'virtualbox' do |v|
+    v.memory = ENV.fetch('RAILS_DEV_BOX_RAM', 4096).to_i
+    v.cpus   = ENV.fetch('RAILS_DEV_BOX_CPUS', 2).to_i
+    v.customize [ "modifyvm", :id, "--uartmode1", "disconnected" ]
+  end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,19 +9,51 @@ function install {
     apt-get -y install "$@" >/dev/null 2>&1
 }
 
+# FIXME: This addresses an issue with Ubuntu 17.10 (Artful Aardvark). Should be
+# revisited when the base image gets upgraded.
+#
+# Workaround for https://bugs.launchpad.net/cloud-images/+bug/1726818 without
+# this the root file system size will be about 2GB.
+echo expanding root file system
+sudo resize2fs /dev/sda1
+
+echo adding swap file
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+
 echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
 install 'development tools' build-essential
 
 install Git git
 install SQLite sqlite3 libsqlite3-dev
+install memcached memcached
+install Redis redis-server
+install RabbitMQ rabbitmq-server
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
 
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
+install 'Blade dependencies' libncurses5-dev
 install 'ExecJS runtime' nodejs
+
+# To generate guides in Kindle format.
+install 'ImageMagick' imagemagick
+echo installing KindleGen
+kindlegen_tarball=kindlegen_linux_2.6_i386_v2_9.tar.gz
+wget -q http://kindlegen.s3.amazonaws.com/$kindlegen_tarball
+tar xzf $kindlegen_tarball kindlegen
+mv kindlegen /usr/local/bin
+rm $kindlegen_tarball
+
+install 'MuPDF' mupdf mupdf-tools
+install 'FFmpeg' ffmpeg
 
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
@@ -42,10 +74,13 @@ echo installing ruby 2.4.2
 sudo -u vagrant -H -i rbenv install 2.4.2 >/dev/null 2>&1
 sudo -u vagrant -H -i rbenv global 2.4.2 >/dev/null 2>&1
 
+echo installing current RubyGems
+sudo -u vagrant gem update --system -N >/dev/null 2>&1
+
 echo installing Bundler
 sudo -u vagrant -H -i gem install bundler -N >/dev/null 2>&1
 
-echo installing Rails 4.0.5
+echo installing Rails 5.1.2
 sudo -u vagrant -H -i gem install rails -N -v '5.1.2' >/dev/null 2>&1
 
 echo cleanup
